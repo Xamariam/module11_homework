20db4f16c9f2b4aa60747c97c3cd1f2920da8d41
Name <email@example.ru>
2023-04-24 16:30:24 +0300

    first commit
     Please enter the commit message for your changes. Lines starting

---
+++ src/repeatWord.js
@@ -0,0 +1,14 @@
+export function repeatWord(word, count) {
+    let words = '';
+    for (let i = 1; i <= count; ++i)
+      {
+       if(i === count){
+        words += word + i + '.';
+        return(words)
+       }
+        words += word + i + ', ';
+        
+      }
+    return(words);
+  }
+  
